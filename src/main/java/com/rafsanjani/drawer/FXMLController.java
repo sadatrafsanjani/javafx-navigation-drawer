8a0a7f6dc65143a6ebe27bce9b0dedc2420e9d29
Rafsanjani <sadatrafsanjani@gmail.com>
2017-04-12 00:03:12 +0600

    First Commit
    Complete navigation drawer using JavaFX

---
+++ src/main/java/com/rafsanjani/drawer/FXMLController.java
@@ -0,0 +1,39 @@
+package com.rafsanjani.drawer;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+public class FXMLController implements Initializable {
+    
+    @FXML
+    private Button menu;
+    @FXML
+    private VBox drawer;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        drawerAction();
+    }  
+    
+    private void drawerAction() {
+    
+        TranslateTransition openNav = new TranslateTransition(new Duration(350), drawer);
+        openNav.setToX(0);
+        TranslateTransition closeNav = new TranslateTransition(new Duration(350), drawer);
+        menu.setOnAction((ActionEvent evt) -> {
+            if (drawer.getTranslateX() != 0) {
+                openNav.play();
+            } else {
+                closeNav.setToX(-(drawer.getWidth()));
+                closeNav.play();
+            }
+        });
+    }
+}
